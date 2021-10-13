0e0fa58259ee2410da757e80f3d72b82b2c745d6
张海赢 <zhyzszz@foxmail.com>
2021-10-13 19:39:02 +0800

    【10-13】2-3 项目基础代码编写

--- src/main.js
+++ src/main.js
@@ -2,5 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'normalize.css'
+
+// 引入全局样式文件
+import '@/assets/scss/index.scss'
 
 createApp(App).use(store).use(router).mount('#app')
---
+++ vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  css: {
+    // 向 CSS 相关的 loader 传递选项
+    loaderOptions: {
+      // 给 sass-loader 传递选项
+      scss: {
+        // 全局引入 variable 和 mixin
+        prependData: `
+          @import "~@/assets/scss/variable.scss";
+          @import "~@/assets/scss/mixin.scss";
+        `
+      }
+    }
+  }
+}
