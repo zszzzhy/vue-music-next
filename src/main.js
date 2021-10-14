40686592d85ba8909b6c616cdd4c6aec8ca58d96
张海赢 <zhyzszz@foxmail.com>
2021-10-14 21:06:05 +0800

    【10-14】2-10 图片懒加载的实现

--- src/main.js
+++ src/main.js
@@ -4,7 +4,12 @@ import router from './router'
 import store from './store'
 import 'normalize.css'
 
+import lazyPlugin from 'vue3-lazy'
+
 // 引入全局样式文件
 import '@/assets/scss/index.scss'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).use(lazyPlugin, {
+  // 懒加载默认图片
+  loading: require('@/assets/images/default.png')
+}).mount('#app')
--- src/views/recommend.vue
+++ src/views/recommend.vue
@@ -12,7 +12,7 @@
           <ul>
             <li class="item" v-for="item in albums" :key="item.id">
               <div class="icon">
-                <img width="60" height="60" :src="item.pic" alt="" />
+                <img width="60" height="60" v-lazy="item.pic" />
               </div>
               <div class="text">
                 <h2 class="name">{{ item.username }}</h2>
