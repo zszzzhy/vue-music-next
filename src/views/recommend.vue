b0d9fd1f13d00776912e3342fc14c8b1f017b3be
张海赢 <zhyzszz@foxmail.com>
2021-10-14 17:05:41 +0800

    【10-14】2-8轮播图组件的使用

--- src/views/recommend.vue
+++ src/views/recommend.vue
@@ -1,29 +1,54 @@
 <template>
   <div class="recommend">
-    榜单页面
+    <div class="slider-wrapper">
+      <div class="slider-content">
+        <Slider v-if="sliders.length" :sliders="sliders" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import { getRecommend } from '@/service/recommend'
+import Slider from '@/components/base/slider/slider'
 
 export default {
   name: 'recommend',
-  setup () {
-    const getList = async () => {
-      const data = await getRecommend()
-      console.log(data)
-    }
-
-    getList()
-
+  components: {
+    Slider
+  },
+  data () {
     return {
-
+      sliders: []
     }
+  },
+  async created () {
+    const result = await getRecommend()
+    this.sliders = result.sliders
+    console.log(result)
   }
 }
 </script>
 
 <style lang="scss" scoped>
-
+.recommend {
+  position: fixed;
+  width: 100%;
+  top: 88px;
+  bottom: 0;
+  .slider-wrapper {
+    position: relative;
+    width: 100%;
+    height: 0;
+    padding-top: 40%;
+    overflow: hidden;
+    .slider-content {
+      position: absolute;
+      left: 0;
+      top: 0;
+      width: 100%;
+      height: 100%;
+    }
+  }
+}
 </style>
