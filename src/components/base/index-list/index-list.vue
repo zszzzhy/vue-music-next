74d1bb218d30da674d2e802690ca763562717c39
张海赢 <zhyzszz@foxmail.com>
2021-10-15 16:48:07 +0800

    【10-15】3-2 IndexList 组件基础滚动功能实现

---
+++ src/components/base/index-list/index-list.vue
@@ -0,0 +1,79 @@
+<template>
+  <scroll class="index-list">
+    <ul>
+      <li v-for="group in data" :key="group.title" class="group">
+        <h2 class="title">{{ group.title }}</h2>
+        <ul>
+          <li v-for="item in group.list" :key="item.id" class="item">
+            <img v-lazy="item.pic" class="avatar" />
+            <span class="name">{{ item.name }}</span>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </scroll>
+</template>
+
+<script>
+import Scroll from '@/components/base/scroll/scroll'
+
+export default {
+  name: 'index-list',
+  components: {
+    Scroll
+  },
+  props: {
+    data: {
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.index-list {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  background: $color-background;
+  ul {
+    padding: 0;
+    .group {
+      list-style: none;
+      padding-bottom: 30px;
+      .title {
+        height: 30px;
+        line-height: 30px;
+        font-size: $font-size-small;
+        color: $color-text-l;
+        background: $color-highlight-background;
+        padding-left: 20px;
+        margin: 0;
+      }
+      ul {
+        padding: 0;
+        .item {
+          display: flex;
+          align-items: center;
+          padding: 20px 0 0 30px;
+          list-style: none;
+          .avatar {
+            width: 50px;
+            height: 50px;
+            border-radius: 50%;
+          }
+          .name {
+            margin-left: 20px;
+            color: $color-text-l;
+            font-size: $font-size-medium;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
--- src/views/singer.vue
+++ src/views/singer.vue
@@ -1,19 +1,36 @@
 <template>
-  <div class="singer"></div>
+  <div class="singer" v-loading="!singers.length">
+    <IndexList :data="singers" />
+  </div>
 </template>
 
 <script>
 import { getSingerList } from '@/service/singer'
+import IndexList from '@/components/base/index-list/index-list'
 
 export default {
   name: 'singer',
+  components: {
+    IndexList
+  },
+  data () {
+    return {
+      singers: []
+    }
+  },
   async created () {
     const result = await getSingerList()
+    this.singers = result.singers
     console.log(result)
   }
 }
 </script>
 
 <style lang="scss" scoped>
-
+.singer {
+  position: fixed;
+  width: 100%;
+  top: 88px;
+  bottom: 0;
+}
 </style>
